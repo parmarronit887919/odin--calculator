20c19df6a038c5fab3a9e518beb2c899d280b565
parmarronit887919 <ronitparmar15@gmail.com>
2024-06-27 18:36:55 +0530

    adding ui to the rock paper scissor game

--- scrpt.js
+++ scrpt.js
@@ -1,7 +1,6 @@
 let values = ["paper", "rock", "scissors"];
 let humanScore = 0;
 let computerScore = 0;
-let gamePlays = 5;
 
 function getComputerChoice() {
     let randomindex = Math.floor(Math.random() * 3);
@@ -9,30 +8,69 @@ function getComputerChoice() {
 
 }
 
-function getHumanChoice() {
-    let choice = prompt("please select between rock, paper or scissors").toLowerCase();
-    while(!values.includes(choice)){
-        choice =prompt("Invalid choice, Please select an option between rock Paper or Scissors").toLowerCase();
-    }
-    return choice;
-}
 
-function playRound() {
+const container = document.querySelector("#container");
+const Result = document.createElement("div");
+const choice = document.createElement("h3");
+const score = document.createElement("h3")
+const winnerDeclaration = document.createElement("h3")
+container.appendChild(Result)
+Result.appendChild(choice);
+Result.appendChild(score);
+Result.appendChild(winnerDeclaration);
 
-    let humanSelection = getHumanChoice();
+
+
+function playRound1() {
+
+    let humanSelection = "rock";
     let computerSelection = getComputerChoice();
-
-    console.log("Your choice", humanSelection)
-    console.log("Computers choice", computerSelection)
 
     if (humanSelection === computerSelection) {
         console.log("tie, both of you think alike!");
     }
-    else if (
-        (humanSelection === "paper" && computerSelection === "rock") ||
-        (humanSelection === "scissors" && computerSelection === "paper") ||
-        (humanSelection === "rock" && computerSelection === "scissors")
-    ) {
+    else if (computerSelection === "scissors") {
+        console.log("You Wn Ths Round!")
+        humanScore++
+    }
+    else {
+        console.log("You Lost This Round")
+        computerScore++
+    }
+    choice.textContent = "Computer Choice Is " + computerSelection;
+    score.textContent = "Your Score: " + humanScore + "\nComputer Score: " + computerScore;
+}
+
+function playRound2() {
+
+    let humanSelection = "paper";
+    let computerSelection = getComputerChoice();
+
+    if (humanSelection === computerSelection) {
+        console.log("tie, both of you think alike!");
+    }
+    else if (computerSelection === "rock") {
+        console.log("You Wn Ths Round!")
+        humanScore++
+    }
+    else {
+        console.log("You Lost This Round")
+        computerScore++
+    }
+    choice.textContent = "Computer Choice Is " + computerSelection;
+    score.textContent = "Your Score: " + humanScore + "\nComputer Score: " + computerScore;
+
+}
+
+function playRound3() {
+
+    let humanSelection = "scissors";
+    let computerSelection = getComputerChoice();
+
+    if (humanSelection === computerSelection) {
+        console.log("tie, both of you think alike!");
+    }
+    else if (computerSelection === "paper") {
         console.log("You Wn Ths Round!")
         humanScore++
     }
@@ -41,20 +79,11 @@ function playRound() {
         computerScore++
     }
 
-    console.log("Your score", humanScore);
-    console.log("Computer Score", computerScore);
-
-    if(humanScore > computerScore){
-        console.log(" You win this game ")
-    }
-    else{
-        console.log(" You lost this game ")
-    }
+    choice.textContent = "Computer Choice Is " + computerSelection;
+    score.textContent = "Your Score: " + humanScore + "Computer Score: " + computerScore;
 }
 
-for(let i =0; i < gamePlays; i++){
-playRound();
-}
+
 
 
 
